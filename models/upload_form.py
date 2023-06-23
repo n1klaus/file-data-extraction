dfc176c81b1229a3c0fb9e4d74ba0122a7332fb7
Nick Nyanjui <nicknyanjui@gmail.com>
2023-06-23 15:42:15 +0300

    Update supported file types

--- models/upload_form.py
+++ models/upload_form.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 
-""""""
+"""Module to define class model for upload form"""
 
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 
-class UploadForm(FlaskForm):
-    """"""
-    upload = FileField('Upload', 
-                      validators=[
-                            FileRequired(), 
-                            FileAllowed(['pdf', 'png', 'jpg', 'jpeg'], 'Unsupported file type!')
-                                ]
-                    )
 
+class UploadForm(FlaskForm):
+    """Definition for upload form model"""
+    upload = FileField('Upload',
+                       validators=[
+                           FileRequired(),
+                           FileAllowed(['pdf', 'png', 'jpg', 'jpeg', 'doc', 'txt'],
+                                       'Unsupported file type!')
+                       ]
+                       )
